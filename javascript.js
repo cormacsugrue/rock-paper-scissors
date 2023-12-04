e0dbc37202fdfb7a3953cdb3ba5caa947c02cda1
Cormac <sugrue.cormac@gmail.com>
2023-12-04 15:25:23 +1100

    Add check to getPlayerSelection for legal input

--- javascript.js
+++ javascript.js
@@ -1,21 +1,8 @@
-
-
 let playerScore = 0;
 let computerScore = 0; 
-
-// instantiate currentRoundResult assign Null
 let currentRoundResult = null;
 
-
-// Instantiate and set computerSelection using function getComputerSelection 
-// Instantiate STRINGG humanSelection 
-
 // Create function getComputerSelection 
-//     CREATE variable rand and assign random integer from 0 to 1
-//     SWITCH(rand)
-//         rand is 0 return string "Rock"
-//         rand is 1 return string "Paper"
-//         rand is 2 return string "Scissors"
 function getComputerSelection(){
     let rand = Math.round(Math.random()*2);
     // console.log(rand);
@@ -27,35 +14,22 @@ function getComputerSelection(){
         case 2:
             return "Scissors";
     }
-        
 }
 
-// CREATE funtion getPlayerSelecton
-//     prompt user "Play your hand:"
+// CREATE function getPlayerSelecton
 function getPlayerSelection(){
     let playerSelection = prompt("Play your hand:");
     // Converts all user input to initial letter uppercase and rest of string lowercase
     playerSelection = playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1).toLocaleLowerCase();
     // console.log(playerSelection);
-    return playerSelection
+    if((playerSelection != "Rock") && (playerSelection != "Paper") && (playerSelection != "Scissors")){
+        alert("invalid input! Please try again.");
+        getPlayerSelection();
+    }
+    else return playerSelection;
 }
 
 // Create function playRound which takes 2 arguments of type string "computer, player"
-//     if player equals computer
-//         return zero 
-//     else if (
-//         (player equals rock && computer equals paper) 
-//         || (player equals paper && computer equals scissors)
-//         || (player equals scissors && computer equals rock)
-//         ){    
-//             //computerScore++; 
-//             return "Player wins"
-//         }
-//     else {
-            
-//             //return playerScore++;
-//             return "computer wins"
-//     }
 function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
         alert("Draw, play again!")
@@ -73,21 +47,7 @@ function playRound(playerSelection, computerSelection){
     }
     
 }
-// console.log(playRound("Scissor", "Rock"));
 // Create a function that loops through 5 rounds of the game
-    // WHILE currentRoundResult equals "Draw" or currentRoundResult equals Null
-    //     ASSIGN currentRoundResult TO playRound(getComputerSelection(), getHumanSelection()) 
-    //     IF  currentRoundResult equals "Draw"
-    //         print to console "Draw"
-    //     Else IF currentRoundResult equals "Player Wins"
-    //         print to console Player "player wins"
-    //     Else 
-    //         print computer wins
-    //     END
-    // Compare the value of humanScore to playerscore to determine the winner
-
-
-
 function game(){
 
     for(let i = 0; i < 5; i++){
@@ -96,7 +56,6 @@ function game(){
             // console.log("here");
              console.log(currentRoundResult);
         }
-        // console.log("hello");
         currentRoundResult = null;
 
     }
@@ -109,9 +68,3 @@ function game(){
 }
 
 game();
-
-
-
-
- 
- 
