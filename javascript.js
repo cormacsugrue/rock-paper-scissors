c767f968d0c68d7e74f35718cc62ae9543f4d451
Cormac <sugrue.cormac@gmail.com>
2023-12-10 16:08:00 +1100

    ADD a ui for outcome of previous game

--- javascript.js
+++ javascript.js
@@ -32,6 +32,15 @@ function getPlayerSelection(){
 // Takes 2 arguments of type string "computer, player" , in the case of a draw the game will be replayed until there is a winner. Returns result string
 function playRound(event){
     
+    const drawAlert = document.querySelector('#drawAlert');
+    const winner = document.querySelector('#winner');
+
+    // reset winner 
+    winner.textContent = "";
+
+    // set draw alert to empty string
+    drawAlert.textContent = '';
+
     // Get player selection from the event element
     const playerSelection = event.target.id;
     let computerSelection = getComputerSelection();
@@ -40,21 +49,39 @@ function playRound(event){
 
 
     if (playerSelection == computerSelection){
-        // alert("Draw, play again!")
-        
+        // alert("Draw")
+        drawAlert.textContent = `Draw! Both chose ${playerSelection} `
     } else if ( 
         (playerSelection == "rock" && computerSelection == "paper")
         || (playerSelection == "paper" && computerSelection == "scissors")
-        || (playerSelection == "scissor" && computerSelection == "rock")
+        || (playerSelection == "scissors" && computerSelection == "rock")
     ) {
         computerScore++;
         // console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+        drawAlert.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
     } else {
         playerScore++;
-        console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        drawAlert.textContent = `You Win! ${playerSelection} beats ${computerSelection}`
+
     }
 
-    result.textContent = `Score: Player: ${playerScore}, Computer: ${computerScore}`;
+    
+    result.children[0].lastChild.textContent = playerScore;
+    result.children[1].lastChild.textContent = computerScore;
+    
+
+    // reset round an or game alerts
+
+    // check for winner (first to 5)
+    if (playerScore === 5 ) {
+        winner.textContent = "Player Wins!"
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5) {
+        winner.textContent = "Computer Wins!"
+        playerScore = 0;
+        computerScore = 0;
+    } 
 
 
 }
