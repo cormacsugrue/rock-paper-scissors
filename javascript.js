d55e8271389e2c13e9b898e4cefba934b39d7c5c
Cormac <sugrue.cormac@gmail.com>
2023-12-10 16:13:32 +1100

    REMOVE unnecessary functions and variables

--- javascript.js
+++ javascript.js
@@ -16,18 +16,6 @@ function getComputerSelection(){
     }
 }
 
-// CREATE function getPlayerSelecton
-function getPlayerSelection(){
-    let playerSelection = prompt("Play your hand:");
-    // Converts all user input to initial letter uppercase and rest of string lowercase
-    playerSelection = playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1).toLowerCase();
-    // Check if player input is correct;
-    if((playerSelection != "Rock") && (playerSelection != "Paper") && (playerSelection != "Scissors")){
-        alert("invalid input! Please try again.");
-        playerSelection = getPlayerSelection();
-    }
-    return playerSelection;
-}
 
 // Takes 2 arguments of type string "computer, player" , in the case of a draw the game will be replayed until there is a winner. Returns result string
 function playRound(event){
@@ -86,27 +74,6 @@ function playRound(event){
 
 }
         
-    
-
-// Loops through 5 rounds of the game and displays results for each round and final winner
-function game(){
-
-    for(let i = 0; i < 5; i++){
-        currentRoundResult = playRound(getPlayerSelection(), getComputerSelection());
-        console.log(currentRoundResult);
-        currentRoundResult = null;
-    }
-
-    if(playerScore > computerScore){
-        console.log("Player wins the game " + playerScore + " - " + computerScore);
-    } else {
-        console.log("Computer wins the game " + computerScore + " - " + playerScore);
-    }
-}
-
-// game();
-
-
 const btnRock = document.querySelector('#rock');
 const btnPaper = document.querySelector('#paper');
 const btnScissors = document.querySelector('#scissors');
